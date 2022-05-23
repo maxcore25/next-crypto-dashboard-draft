046ff6b63774d44529668567e5200a5ac01fce5b
maxcore25 <mkorobov2002@mail.ru>
2022-05-23 16:08:57 +0300

    Material UI Icons Installed

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,13 @@
 import CurrencyConverter from '../app/components/CurrencyConverter';
 import CustomHead from '../app/components/CustomHead';
 import NewsFeed from '../app/components/NewsFeed';
-import styles from '../styles/Home.module.scss';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <>
       <CustomHead />
       <CurrencyConverter />
       <NewsFeed />
-    </div>
+    </>
   );
 }
