201397a1141a3fd1e5a1a1dad8d78aa79b782b13
maxcore25 <mkorobov2002@mail.ru>
2022-05-23 18:53:29 +0300

    ExchangeRate Stylized

--- app/components/elements/ExchangeRate/index.js
+++ app/components/elements/ExchangeRate/index.js
@@ -3,9 +3,11 @@ import { Paper } from '@mui/material';
 
 const ExchangeRate = () => {
   return (
-    <Paper elevation={3}>
+    <Paper elevation={3} sx={{ borderRadius: '12px' }}>
       <div className={styles.mainGrid}>
-        <h2>ExchangeRate</h2>
+        <h2>Exchange Rate</h2>
+        <h3>123.989</h3>
+        <span>BTC to USD</span>
       </div>
     </Paper>
   );
