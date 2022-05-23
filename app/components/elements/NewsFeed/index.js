e1bc05ad96b03f06bed1456ad84470099fc3e03a
maxcore25 <mkorobov2002@mail.ru>
2022-05-23 23:53:35 +0300

    News Link Tag Added

--- app/components/elements/NewsFeed/index.js
+++ app/components/elements/NewsFeed/index.js
@@ -7,12 +7,14 @@ const NewsFeed = () => {
       <div className={styles.mainGrid}>
         <h2>News Feed</h2>
         <div className={styles.newsGrid}>
-          <Paper
-            elevation={3}
-            sx={{ borderRadius: '12px' }}
-            className={styles.newsCard}>
-            <h4>Hello</h4>
-          </Paper>
+          <a href=''>
+            <Paper
+              elevation={3}
+              sx={{ borderRadius: '12px' }}
+              className={styles.newsCard}>
+              <h4>Hello</h4>
+            </Paper>
+          </a>
         </div>
       </div>
     </Paper>
