4569c1f76a857ffe36a3dd1ee3cc45b5fdb7b8de
maxcore25 <mkorobov2002@mail.ru>
2022-05-23 18:44:49 +0300

    ExchangeRate Styles File Created

--- app/components/elements/ExchangeRate/index.js
+++ app/components/elements/ExchangeRate/index.js
@@ -1,7 +1,14 @@
+import styles from './ExchangeRate.module.scss';
 import { Paper } from '@mui/material';
 
 const ExchangeRate = () => {
-  return <Paper elevation={3}>ExchangeRate</Paper>;
+  return (
+    <Paper elevation={3}>
+      <div className={styles.mainGrid}>
+        <h2>ExchangeRate</h2>
+      </div>
+    </Paper>
+  );
 };
 
 export default ExchangeRate;
