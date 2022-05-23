2be2cf0b2790e555e8381370406a18c0366a7da3
maxcore25 <mkorobov2002@mail.ru>
2022-05-23 16:19:20 +0300

    MainLayout Styles Created

--- app/components/layouts/MainLayout/index.js
+++ app/components/layouts/MainLayout/index.js
@@ -1,14 +1,15 @@
+import styles from './MainLayout.module.scss';
 import CustomHead from '../../elements/CustomHead';
 import NewsFeed from '../../elements/NewsFeed';
 import CurrencyConverter from '../../elements/CurrencyConverter';
 
 const MainLayout = () => {
   return (
-    <>
+    <div className={styles.block}>
       <CustomHead />
       <CurrencyConverter />
       <NewsFeed />
-    </>
+    </div>
   );
 };
 
