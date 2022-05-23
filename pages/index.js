8d05781c2845e50743e4e32b5b0b29f3e4ccba52
maxcore25 <mkorobov2002@mail.ru>
2022-05-23 16:10:46 +0300

    Components To Elements Folder Moved

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,3 @@
-import CurrencyConverter from '../app/components/CurrencyConverter';
-import CustomHead from '../app/components/CustomHead';
-import NewsFeed from '../app/components/NewsFeed';
-
 export default function Home() {
-  return (
-    <>
-      <CustomHead />
-      <CurrencyConverter />
-      <NewsFeed />
-    </>
-  );
+  return <></>;
 }
