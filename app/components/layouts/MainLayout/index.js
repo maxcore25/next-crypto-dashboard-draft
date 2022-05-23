74afe37041b41db17a3c92d7d89a79328bf020b3
maxcore25 <mkorobov2002@mail.ru>
2022-05-23 16:17:39 +0300

    Components Into MainLayout Added

--- app/components/layouts/MainLayout/index.js
+++ app/components/layouts/MainLayout/index.js
@@ -1,5 +1,15 @@
+import CustomHead from '../../elements/CustomHead';
+import NewsFeed from '../../elements/NewsFeed';
+import CurrencyConverter from '../../elements/CurrencyConverter';
+
 const MainLayout = () => {
-  return <></>;
+  return (
+    <>
+      <CustomHead />
+      <CurrencyConverter />
+      <NewsFeed />
+    </>
+  );
 };
 
 export default MainLayout;
