eb6b0b37798d553b98e91acb7d7baacd59b8cc1f
maxcore25 <mkorobov2002@mail.ru>
2022-05-23 17:24:27 +0300

    Temporary JSX Added

--- app/components/elements/CurrencyConverter/index.js
+++ app/components/elements/CurrencyConverter/index.js
@@ -12,7 +12,51 @@ const CurrencyConverter = () => {
           gridTemplateRows: { md: '1fr 1fr' },
           gap: 2,
         }}>
-        <div>CurrencyConverter</div>
+        <div>
+          <table>
+            <tbody>
+              <tr>
+                <td>Primary Currency:</td>
+                <td>
+                  <input
+                    type='number'
+                    name='currency-amount-1'
+                    value={''}
+                    onChange={e => console.log(e.target.value)}
+                  />
+                </td>
+                <td>
+                  <select
+                    value={'chosenPrimaryCurrency'}
+                    name='currency-option-1'
+                    className='currency-options'
+                    onChange={e => console.log(e.target.value)}>
+                    <option>{'currency'}</option>
+                  </select>
+                </td>
+              </tr>
+              <tr>
+                <td>Secondary Currency:</td>
+                <td>
+                  <input
+                    name='currency-amount-2'
+                    value={'result'}
+                    disabled={true}
+                  />
+                </td>
+                <td>
+                  <select
+                    value={'chosenSecondaryCurrency'}
+                    name='currency-option-2'
+                    className='currency-options'
+                    onChange={e => console.log(e.target.value)}>
+                    <option>{'currency'}</option>
+                  </select>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
         <ExchangeRate />
       </Box>
     </Paper>
