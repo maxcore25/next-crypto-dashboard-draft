b6f6ce3debbca3cffa8bc4df2fa884a2333c8f35
maxcore25 <mkorobov2002@mail.ru>
2022-05-23 19:05:48 +0300

    NewsFeed Stylized

--- app/components/elements/NewsFeed/index.js
+++ app/components/elements/NewsFeed/index.js
@@ -1,7 +1,22 @@
+import { Paper } from '@mui/material';
 import styles from './NewsFeed.module.scss';
 
 const NewsFeed = () => {
-  return <div>NewsFeed</div>;
+  return (
+    <Paper elevation={3} sx={{ borderRadius: '12px' }}>
+      <div className={styles.mainGrid}>
+        <h2>News Feed</h2>
+        <div className={styles.newsGrid}>
+          <Paper
+            elevation={3}
+            sx={{ borderRadius: '12px' }}
+            className={styles.newsCard}>
+            <h4>Hello</h4>
+          </Paper>
+        </div>
+      </div>
+    </Paper>
+  );
 };
 
 export default NewsFeed;
