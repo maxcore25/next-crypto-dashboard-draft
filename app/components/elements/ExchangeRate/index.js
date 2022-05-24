7e6b93929eab5d73ed12b8dc627b063c65550970
maxcore25 <mkorobov2002@mail.ru>
2022-05-24 21:43:19 +0300

    'exchangeRate' Prop Into ExchangeRate Component Added

--- app/components/elements/ExchangeRate/index.js
+++ app/components/elements/ExchangeRate/index.js
@@ -1,12 +1,12 @@
 import styles from './ExchangeRate.module.scss';
 import { Paper } from '@mui/material';
 
-const ExchangeRate = () => {
+const ExchangeRate = ({ exchangeRate }) => {
   return (
     <Paper elevation={3} sx={{ borderRadius: '12px' }}>
       <div className={styles.mainGrid}>
         <h2>Exchange Rate</h2>
-        <h3>123.989</h3>
+        <h3>{exchangeRate}</h3>
         <span>BTC to USD</span>
       </div>
     </Paper>
