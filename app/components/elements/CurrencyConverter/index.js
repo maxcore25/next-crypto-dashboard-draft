c081c78cc5ff71d1a1d0afb245a7cac577efd0c0
maxcore25 <mkorobov2002@mail.ru>
2022-05-24 20:06:50 +0300

    Convert Function Created And Alpha Vantage API Boilerplate Added

--- app/components/elements/CurrencyConverter/index.js
+++ app/components/elements/CurrencyConverter/index.js
@@ -11,11 +11,39 @@ import { Box } from '@mui/system';
 import { useState } from 'react';
 import ExchangeRate from '../ExchangeRate';
 import { CURRENCIES } from '../../../constants';
+import axios from 'axios';
 
 const CurrencyConverter = () => {
   const [chosenPrimaryCurrency, setChosenPrimaryCurrency] = useState('BTC');
   const [chosenSecondaryCurrency, setChosenSecondaryCurrency] = useState('USD');
   const [amount, setAmount] = useState(1);
+
+  const convert = () => {
+    const options = {
+      method: 'GET',
+      url: 'https://alpha-vantage.p.rapidapi.com/query',
+      params: {
+        interval: '5min',
+        function: 'TIME_SERIES_INTRADAY',
+        symbol: 'MSFT',
+        datatype: 'json',
+        output_size: 'compact',
+      },
+      headers: {
+        'X-RapidAPI-Host': 'alpha-vantage.p.rapidapi.com',
+        'X-RapidAPI-Key': 'b37d95749fmsh35d40cd8dacf8e9p1560f4jsn5a6f578e52ac',
+      },
+    };
+
+    axios
+      .request(options)
+      .then(function (response) {
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
 
   return (
     <Paper elevation={3} sx={{ borderRadius: '12px' }}>
@@ -77,7 +105,9 @@ const CurrencyConverter = () => {
                 </FormControl>
               </Box>
             </div>
-            <Button variant='contained'>Convert</Button>
+            <Button onClick={() => convert()} variant='contained'>
+              Convert
+            </Button>
           </div>
         </Paper>
         <ExchangeRate />
