ea62231c66e083da5bec30bb981451c8393ee107
maxcore25 <mkorobov2002@mail.ru>
2022-05-24 22:11:34 +0300

    Crypto News API Boilerplate Into NewsFeed Added

--- app/components/elements/NewsFeed/index.js
+++ app/components/elements/NewsFeed/index.js
@@ -1,7 +1,29 @@
-import { Paper } from '@mui/material';
 import styles from './NewsFeed.module.scss';
+import { Paper } from '@mui/material';
+import { useEffect } from 'react';
+import axios from 'axios';
 
 const NewsFeed = () => {
+  useEffect(() => {
+    const options = {
+      method: 'GET',
+      url: 'https://crypto-news-live3.p.rapidapi.com/news',
+      headers: {
+        'X-RapidAPI-Host': 'crypto-news-live3.p.rapidapi.com',
+        'X-RapidAPI-Key': 'b37d95749fmsh35d40cd8dacf8e9p1560f4jsn5a6f578e52ac',
+      },
+    };
+
+    axios
+      .request(options)
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+
   return (
     <Paper elevation={3} sx={{ borderRadius: '12px' }}>
       <div className={styles.mainGrid}>
