1c47fd2bcbc067cab74ca8599bceb260d6b0b1d9
maxcore25 <mkorobov2002@mail.ru>
2022-05-25 08:18:08 +0300

    MUI Switch Theme Button Added

--- app/components/modules/Header/index.js
+++ app/components/modules/Header/index.js
@@ -1,4 +1,6 @@
 import styles from './Header.module.scss';
+import { IconButton } from '@mui/material';
+import LightModeIcon from '@mui/icons-material/LightMode';
 
 const Header = () => {
   return (
@@ -7,6 +9,9 @@ const Header = () => {
         <span>Lorem</span>
         <span>Lorem</span>
         <span>Lorem</span>
+        <IconButton color='primary' aria-label='Switch Theme' component='span'>
+          <LightModeIcon />
+        </IconButton>
       </div>
     </header>
   );
