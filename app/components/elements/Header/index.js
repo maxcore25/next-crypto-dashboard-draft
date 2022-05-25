f6983926fd2be33744a5b6bb10932e752558d182
maxcore25 <mkorobov2002@mail.ru>
2022-05-25 08:08:51 +0300

    Header Stylized

--- app/components/elements/Header/index.js
+++ app/components/elements/Header/index.js
@@ -1,7 +1,15 @@
 import styles from './Header.module.scss';
 
 const Header = () => {
-  return <header className={styles.header}>Enter</header>;
+  return (
+    <header className={styles.header}>
+      <div className={styles.contentContainer}>
+        <span>Lorem</span>
+        <span>Lorem</span>
+        <span>Lorem</span>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
