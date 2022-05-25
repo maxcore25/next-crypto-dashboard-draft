33b070909a05280517e7b7dd049a26cf2284b81f
maxcore25 <mkorobov2002@mail.ru>
2022-05-25 08:23:25 +0300

    Switch Theme Button Logic Written

--- app/components/modules/Header/index.js
+++ app/components/modules/Header/index.js
@@ -1,16 +1,28 @@
 import styles from './Header.module.scss';
 import { IconButton } from '@mui/material';
 import LightModeIcon from '@mui/icons-material/LightMode';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import { useState } from 'react';
 
 const Header = () => {
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+  const handleSwitchTheme = () => {
+    setIsDarkTheme(!isDarkTheme);
+  };
+
   return (
     <header className={styles.header}>
       <div className={styles.contentContainer}>
         <span>Lorem</span>
         <span>Lorem</span>
         <span>Lorem</span>
-        <IconButton color='primary' aria-label='Switch Theme' component='span'>
-          <LightModeIcon />
+        <IconButton
+          onClick={() => handleSwitchTheme()}
+          color='primary'
+          aria-label='Switch Theme'
+          component='span'>
+          {isDarkTheme ? <DarkModeIcon /> : <LightModeIcon />}
         </IconButton>
       </div>
     </header>
