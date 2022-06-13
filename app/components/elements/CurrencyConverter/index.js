72b531fd8b05a8c0316968197bbf0b5f87bd9173
maxcore25 <mkorobov2002@mail.ru>
2022-06-13 19:50:18 +0300

    Exchange Rate Request Data Tested

--- app/components/elements/CurrencyConverter/index.js
+++ app/components/elements/CurrencyConverter/index.js
@@ -27,43 +27,52 @@ const CurrencyConverter = () => {
   });
 
   const convert = () => {
-    const options = {
-      method: 'GET',
-      url: 'https://alpha-vantage.p.rapidapi.com/query',
-      params: {
-        from_currency: chosenPrimaryCurrency,
-        function: 'CURRENCY_EXCHANGE_RATE',
-        to_currency: chosenSecondaryCurrency,
-      },
-      headers: {
-        'X-RapidAPI-Host': 'alpha-vantage.p.rapidapi.com',
-        'X-RapidAPI-Key': process.env.NEXT_PUBLIC_RAPID_API_KEY,
-      },
-    };
-
     axios
-      .request(options)
-      .then(response => {
-        console.log('NewsFeed api:', response.data);
-        setExchangeRate(
-          response.data['Realtime Currency Exchange Rate']['5. Exchange Rate']
-        );
-        setResult(
-          response.data['Realtime Currency Exchange Rate']['5. Exchange Rate'] *
-            amount
-        );
-        setExchangedData({
-          chosenPrimaryCurrency,
-          chosenSecondaryCurrency,
-          exchangeRate:
-            response.data['Realtime Currency Exchange Rate'][
-              '5. Exchange Rate'
-            ],
-        });
+      .post('/api/exchange-rate', {
+        from_currency: chosenPrimaryCurrency,
+        to_currency: chosenSecondaryCurrency,
       })
-      .catch(error => {
-        console.error(error);
+      .then(response => {
+        console.log('CurrencyConverter', response.data);
       });
+
+    // const options = {
+    //   method: 'GET',
+    //   url: 'https://alpha-vantage.p.rapidapi.com/query',
+    //   params: {
+    //     from_currency: chosenPrimaryCurrency,
+    //     function: 'CURRENCY_EXCHANGE_RATE',
+    //     to_currency: chosenSecondaryCurrency,
+    //   },
+    //   headers: {
+    //     'X-RapidAPI-Host': 'alpha-vantage.p.rapidapi.com',
+    //     'X-RapidAPI-Key': process.env.NEXT_PUBLIC_RAPID_API_KEY,
+    //   },
+    // };
+
+    // axios
+    //   .request(options)
+    //   .then(response => {
+    //     console.log('CurrencyConverter api:', response.data);
+    //     setExchangeRate(
+    //       response.data['Realtime Currency Exchange Rate']['5. Exchange Rate']
+    //     );
+    //     setResult(
+    //       response.data['Realtime Currency Exchange Rate']['5. Exchange Rate'] *
+    //         amount
+    //     );
+    //     setExchangedData({
+    //       chosenPrimaryCurrency,
+    //       chosenSecondaryCurrency,
+    //       exchangeRate:
+    //         response.data['Realtime Currency Exchange Rate'][
+    //           '5. Exchange Rate'
+    //         ],
+    //     });
+    //   })
+    //   .catch(error => {
+    //     console.error(error);
+    //   });
   };
 
   return (
--- pages/api/exchange-rate.js
+++ pages/api/exchange-rate.js
@@ -1,11 +1,13 @@
+import axios from 'axios';
+
 export default function handler(req, res) {
   const options = {
     method: 'GET',
     url: 'https://alpha-vantage.p.rapidapi.com/query',
     params: {
-      from_currency: chosenPrimaryCurrency,
+      from_currency: req.body.from_currency,
       function: 'CURRENCY_EXCHANGE_RATE',
-      to_currency: chosenSecondaryCurrency,
+      to_currency: req.body.to_currency,
     },
     headers: {
       'X-RapidAPI-Host': 'alpha-vantage.p.rapidapi.com',
@@ -13,25 +15,27 @@ export default function handler(req, res) {
     },
   };
 
-  axios
-    .request(options)
-    .then(response => {
-      console.log(response.data);
-      setExchangeRate(
-        response.data['Realtime Currency Exchange Rate']['5. Exchange Rate']
-      );
-      setResult(
-        response.data['Realtime Currency Exchange Rate']['5. Exchange Rate'] *
-          amount
-      );
-      setExchangedData({
-        chosenPrimaryCurrency,
-        chosenSecondaryCurrency,
-        exchangeRate:
-          response.data['Realtime Currency Exchange Rate']['5. Exchange Rate'],
-      });
-    })
-    .catch(error => {
-      console.error(error);
-    });
+  // axios
+  //   .request(options)
+  //   .then(response => {
+  //     console.log(response.data);
+  //     setExchangeRate(
+  //       response.data['Realtime Currency Exchange Rate']['5. Exchange Rate']
+  //     );
+  //     setResult(
+  //       response.data['Realtime Currency Exchange Rate']['5. Exchange Rate'] *
+  //         amount
+  //     );
+  //     setExchangedData({
+  //       chosenPrimaryCurrency,
+  //       chosenSecondaryCurrency,
+  //       exchangeRate:
+  //         response.data['Realtime Currency Exchange Rate']['5. Exchange Rate'],
+  //     });
+  //   })
+  //   .catch(error => {
+  //     console.error(error);
+  //   });
+
+  res.status(200).json({ message: 'CurrencyConverter' });
 }
