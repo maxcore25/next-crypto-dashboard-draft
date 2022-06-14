1ff93e826d552fd969e1d3e35aee3849556aa10e
maxcore25 <mkorobov2002@mail.ru>
2022-06-14 20:44:22 +0300

    ExchangeRate Theme Colors Added

--- app/components/elements/ExchangeRate/index.js
+++ app/components/elements/ExchangeRate/index.js
@@ -3,11 +3,14 @@ import { Paper } from '@mui/material';
 
 const ExchangeRate = ({ exchangedData }) => {
   return (
-    <Paper elevation={3} sx={{ borderRadius: '12px' }}>
+    <Paper
+      className={styles.muiPaper}
+      elevation={3}
+      sx={{ borderRadius: '12px' }}>
       <div className={styles.mainGrid}>
-        <h2>Exchange Rate</h2>
-        <h3>{exchangedData.exchangeRate}</h3>
-        <span>
+        <h2 className={styles.title}>Exchange Rate</h2>
+        <h3 className={styles.exchangeRate}>{exchangedData.exchangeRate}</h3>
+        <span className={styles.exchangedData}>
           {exchangedData.chosenPrimaryCurrency} to{' '}
           {exchangedData.chosenSecondaryCurrency}
         </span>
