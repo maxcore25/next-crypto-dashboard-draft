e2246e18de5159128aa4f9bd4bfdd9e451b65e37
maxcore25 <mkorobov2002@mail.ru>
2022-06-14 13:29:57 +0300

    handleSwitchTheme Into MainLayoutContextProvider Added

--- app/context/MainLayoutContextProvider.js
+++ app/context/MainLayoutContextProvider.js
@@ -1,8 +1,29 @@
+import { useState } from 'react';
 import { MainLayoutContext } from '.';
 
 const MainLayoutContextProvider = ({ children }) => {
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+  const handleSwitchTheme = () => {
+    setIsDarkTheme(!isDarkTheme);
+
+    if (isDarkTheme) {
+      document.documentElement.setAttribute('data-theme', 'dark');
+      window.localStorage.setItem(
+        LOCAL_STORAGE_VARIABLES.CRYPTO_DASHBOARD_THEME,
+        'dark'
+      );
+    } else {
+      document.documentElement.setAttribute('data-theme', 'light');
+      window.localStorage.setItem(
+        LOCAL_STORAGE_VARIABLES.CRYPTO_DASHBOARD_THEME,
+        'light'
+      );
+    }
+  };
+
   return (
-    <MainLayoutContext.Provider value={{}}>
+    <MainLayoutContext.Provider value={{ isDarkTheme, handleSwitchTheme }}>
       {children}
     </MainLayoutContext.Provider>
   );
