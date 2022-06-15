69207ac7c20b073382736a15aef49cd5dd74a25c
maxcore25 <mkorobov2002@mail.ru>
2022-06-15 23:02:14 +0300

    CurrencyConverter Styles Fixed

--- app/components/modules/CurrencyConverterBlock/index.js
+++ app/components/modules/CurrencyConverterBlock/index.js
@@ -12,6 +12,7 @@ import { useState } from 'react';
 import { CURRENCIES } from '../../../constants';
 import axios from 'axios';
 import ExchangeRate from '../../elements/ExchangeRate';
+import CurrencyConverter from '../../elements/CurrencyConverter';
 
 const CurrencyConverterBlock = () => {
   const [chosenPrimaryCurrency, setChosenPrimaryCurrency] = useState('BTC');
@@ -50,76 +51,7 @@ const CurrencyConverterBlock = () => {
       sx={{ borderRadius: '12px' }}>
       <div className={styles.mainGrid}>
         <h2 className={styles.title}>Currency Converter</h2>
-        <Paper
-          className={styles.muiPaperInputBlock}
-          elevation={3}
-          sx={{ borderRadius: '12px', p: '16px' }}>
-          <div className={styles.inputsGrid}>
-            <div className={styles.inputContainer}>
-              <TextField
-                className={styles.muiInput}
-                value={amount}
-                onChange={e => setAmount(e.target.value)}
-                id='outlined-basic'
-                label='Currency 1'
-                variant='outlined'
-                inputProps={{
-                  name: 'currency-amount-1',
-                }}
-              />
-              <Box sx={{ width: 85 }}>
-                <FormControl fullWidth>
-                  <Select
-                    className={styles.muiSelect}
-                    value={chosenPrimaryCurrency}
-                    onChange={e => setChosenPrimaryCurrency(e.target.value)}
-                    inputProps={{
-                      name: 'currency-option-1',
-                    }}>
-                    {CURRENCIES.map(currency => (
-                      <MenuItem key={currency} value={currency}>
-                        {currency}
-                      </MenuItem>
-                    ))}
-                  </Select>
-                </FormControl>
-              </Box>
-            </div>
-            <div className={styles.inputContainer}>
-              <TextField
-                className={styles.muiInputDisabled}
-                disabled
-                value={result}
-                id='outlined-basic'
-                label='Currency 2'
-                variant='outlined'
-                inputProps={{
-                  name: 'currency-amount-2',
-                }}
-              />
-              <Box sx={{ width: 85 }}>
-                <FormControl fullWidth>
-                  <Select
-                    className={styles.muiSelect}
-                    value={chosenSecondaryCurrency}
-                    onChange={e => setChosenSecondaryCurrency(e.target.value)}
-                    inputProps={{
-                      name: 'currency-option-2',
-                    }}>
-                    {CURRENCIES.map(currency => (
-                      <MenuItem key={currency} value={currency}>
-                        {currency}
-                      </MenuItem>
-                    ))}
-                  </Select>
-                </FormControl>
-              </Box>
-            </div>
-            <Button onClick={() => convert()} variant='contained'>
-              Convert
-            </Button>
-          </div>
-        </Paper>
+        <CurrencyConverter />
         <ExchangeRate exchangedData={exchangedData} />
       </div>
     </Paper>
