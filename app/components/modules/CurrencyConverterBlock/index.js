69b015789f9069419bc25a6b3437bf2267c1ad75
maxcore25 <mkorobov2002@mail.ru>
2022-06-15 23:07:59 +0300

    Small Refactor

--- app/components/modules/CurrencyConverterBlock/index.js
+++ app/components/modules/CurrencyConverterBlock/index.js
@@ -1,17 +1,10 @@
 import styles from './CurrencyConverterBlock.module.scss';
 import { Paper } from '@mui/material';
 import { useState } from 'react';
-import axios from 'axios';
 import ExchangeRate from '../../elements/ExchangeRate';
 import CurrencyConverter from '../../elements/CurrencyConverter';
 
 const CurrencyConverterBlock = () => {
-  const [chosenPrimaryCurrency, setChosenPrimaryCurrency] = useState('BTC');
-  const [chosenSecondaryCurrency, setChosenSecondaryCurrency] = useState('USD');
-  const [amount, setAmount] = useState(1);
-  const [exchangeRate, setExchangeRate] = useState(0);
-  const [result, setResult] = useState(0);
-
   const [exchangedData, setExchangedData] = useState({
     chosenPrimaryCurrency: 'BTC',
     chosenSecondaryCurrency: 'USD',
