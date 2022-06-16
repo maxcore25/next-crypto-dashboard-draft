67c76e0113bd4bd2e81caa22dc8ad83324b5fef1
maxcore25 <mkorobov2002@mail.ru>
2022-06-16 11:05:39 +0300

    NewsFeedBlock Created

---
+++ app/components/modules/NewsFeedBlock/index.js
@@ -0,0 +1,40 @@
+import styles from './NewsFeedBlock.module.scss';
+import { Paper } from '@mui/material';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const NewsFeedBlock = () => {
+  const [articles, setArticles] = useState();
+
+  // useEffect(() => {
+  //   axios.get('/api/crypto-news').then(response => {
+  //     console.log('NewsFeed api:', response.data);
+  //     setArticles(response.data.slice(0, 7));
+  //   });
+  // }, []);
+
+  return (
+    <Paper
+      className={styles.muiPaper}
+      elevation={3}
+      sx={{ borderRadius: '12px' }}>
+      <div className={styles.mainGrid}>
+        <h2 className={styles.title}>News Feed</h2>
+        <div className={styles.newsGrid}>
+          {articles?.map((article, index) => (
+            <a key={index} href={article.url} className={styles.newsCardLink}>
+              <Paper
+                elevation={3}
+                sx={{ borderRadius: '12px' }}
+                className={styles.newsCard}>
+                <h4 className={styles.cardTitle}>{article.title}</h4>
+              </Paper>
+            </a>
+          ))}
+        </div>
+      </div>
+    </Paper>
+  );
+};
+
+export default NewsFeedBlock;
