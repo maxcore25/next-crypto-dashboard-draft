ec92f6c1eba534170506baedd22803224192f1d5
maxcore25 <mkorobov2002@mail.ru>
2022-06-16 13:10:48 +0300

    Header Animated

--- app/components/modules/Header/index.js
+++ app/components/modules/Header/index.js
@@ -4,12 +4,27 @@ import LightModeIcon from '@mui/icons-material/LightMode';
 import DarkModeIcon from '@mui/icons-material/DarkMode';
 import { useContext } from 'react';
 import { MainLayoutContext } from '../../../context';
+import { motion } from 'framer-motion';
+
+const pVariants = {
+  hidden: { y: '-100%' },
+  visible: {
+    y: 0,
+    transition: {
+      duration: 0.5,
+    },
+  },
+};
 
 const Header = () => {
   const { isDarkTheme, handleSwitchTheme } = useContext(MainLayoutContext);
 
   return (
-    <header className={styles.header}>
+    <motion.header
+      className={styles.header}
+      variants={pVariants}
+      initial='hidden'
+      animate='visible'>
       <div className={styles.contentContainer}>
         <a href='https://github.com/maxcore25/next-crypto-dashboard-draft'>
           GitHub
@@ -22,7 +37,7 @@ const Header = () => {
           {isDarkTheme ? <LightModeIcon /> : <DarkModeIcon />}
         </IconButton>
       </div>
-    </header>
+    </motion.header>
   );
 };
 
