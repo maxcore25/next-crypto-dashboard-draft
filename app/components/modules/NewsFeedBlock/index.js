ebfebaf075eee571dc43f9a677de24b625f1f7c4
maxcore25 <mkorobov2002@mail.ru>
2022-06-16 11:45:36 +0300

    MUI Button Into NewsFeedBlock Added

--- app/components/modules/NewsFeedBlock/index.js
+++ app/components/modules/NewsFeedBlock/index.js
@@ -1,5 +1,5 @@
 import styles from './NewsFeedBlock.module.scss';
-import { Paper } from '@mui/material';
+import { Button, Paper } from '@mui/material';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import NewsCard from '../../elements/NewsCard';
@@ -7,12 +7,12 @@ import NewsCard from '../../elements/NewsCard';
 const NewsFeedBlock = () => {
   const [articles, setArticles] = useState();
 
-  // useEffect(() => {
-  //   axios.get('/api/crypto-news').then(response => {
-  //     console.log('NewsFeed api:', response.data);
-  //     setArticles(response.data.slice(0, 7));
-  //   });
-  // }, []);
+  const getNews = () => {
+    axios.get('/api/crypto-news').then(response => {
+      console.log('NewsFeed api:', response.data);
+      setArticles(response.data.slice(0, 7));
+    });
+  };
 
   return (
     <Paper
@@ -21,6 +21,9 @@ const NewsFeedBlock = () => {
       sx={{ borderRadius: '12px' }}>
       <div className={styles.mainGrid}>
         <h2 className={styles.title}>News Feed</h2>
+        <Button onClick={() => getNews()} variant='contained'>
+          Get News
+        </Button>
         <div className={styles.newsGrid}>
           {articles?.map((article, index) => (
             <NewsCard key={index} article={article} />
